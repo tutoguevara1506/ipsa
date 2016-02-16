23855ae63247b545f769ffb185393561c57555e3
Roberto Ramirez <rramirezech@hotmail.com>
2016-02-16 09:55:14 -0600

    Modificación para calendarizacion de mantenimientos (Prueba) Funcionando al 50%

--- src/java/paquetes/AlertSchedule.java
+++ src/java/paquetes/AlertSchedule.java
@@ -19,14 +19,14 @@ public class AlertSchedule {
 
     private final Logger log = Logger.getLogger(getClass().getName());
     
-    @Schedule(hour = "*", dayOfWeek = "*", persistent=true , info = "Todos los dias cada hora")
+    @Schedule(hour = "*", dayOfWeek = "*", info = "Todos los dias cada hora")
     //@Schedule(second = "*", minute = "*/5", hour = "*", info = "cada 5 minutos")
 
     public void performTask() throws EmailException {
 
         long timeInit = System.currentTimeMillis();
         
-        log.info(":. Inicio TareaProgramada.");
+        log.info(":. Inicio TareaProgramada cada hora.");
         
         Email email = new SimpleEmail();
         email.setHostName("smtp.gmail.com");
